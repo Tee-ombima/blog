d0df5496d81a1a7c8c144ee7339e02ca525aab2a
Tee-ombima <80878881+Tee-ombima@users.noreply.github.com>
2023-05-05 15:17:01 +0300

    Update settings.py

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
-        'HOST': os.getenv('DB_HOST').split(","),
+        'HOST': os.getenv('DB_HOST'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
     }
